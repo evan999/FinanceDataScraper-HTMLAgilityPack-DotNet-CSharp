aa87b398ade177294111d9d1326a953e25ec8fce
evan999 <emeshberge@gmail.com>
2019-08-10 23:31:19 -0400

    Import scrape to database. Imports successfully. Removed misc characters from change value string.

--- FinanceScraper_HTMLAgilityPack/Scraper.cs
+++ FinanceScraper_HTMLAgilityPack/Scraper.cs
@@ -28,7 +28,7 @@ namespace FinanceScraper_HTMLAgilityPack
                     Console.WriteLine("Name: " + nameValue);
                     string indexValue = tableRow.SelectSingleNode("td[3]").InnerText;
                     Console.WriteLine("Index value: " + indexValue);
-                    string changeValue = tableRow.SelectSingleNode("td[4]").InnerText.Replace("&nbsp;", "").Replace("&#9660;", " ");
+                    string changeValue = tableRow.SelectSingleNode("td[4]").InnerText.Replace("&nbsp;", "").Replace("&#9660;", " ").Replace("&#9650;", " ");
                     string[] changeString = changeValue.Split(' ');
 
                     string changeRate = changeString[0];
@@ -38,15 +38,13 @@ namespace FinanceScraper_HTMLAgilityPack
                     if (changeString.Length == 2)
                     {
                         netPercentValue = changeString[1];
-                        Console.WriteLine("Net Percent Rate: " + netPercentValue);                       
+                        Console.WriteLine("Net Percent Rate: " + netPercentValue);
                     }
                     else
                     {
                         netPercentValue = "-";
                         Console.WriteLine("Net Percent Rate: " + netPercentValue);
                     }
-                    // Console.WriteLine("Change value: " + changeValue);
-                    // int changeLength = changeValue.Length;
                     string highIndexValue = tableRow.SelectSingleNode("td[5]").InnerText;
                     Console.WriteLine("High Index Value: " + highIndexValue);
                     string lowIndexValue = tableRow.SelectSingleNode("td[6]").InnerText;
@@ -55,6 +53,7 @@ namespace FinanceScraper_HTMLAgilityPack
                     DateTime timeStampValue = DateTime.Now;
                     Console.WriteLine("Timestamp: " + timeStampValue);
 
+                    Console.WriteLine(" ");
                     
                     var stockRecord = new NasdaqStock
                     {
@@ -68,91 +67,8 @@ namespace FinanceScraper_HTMLAgilityPack
                         Timestamp = timeStampValue
                     };
                     context.NasdaqStocks.Add(stockRecord);
-                    context.SaveChanges();
-                    
-                    // Console.WriteLine("change length: " + changeLength);
-
-
-
-
-
-                    //int cutString = 4;
-
-                    // string change = changeValue.Substring(0, cutString);
-                    // Console.WriteLine("change: " + change);
-
-
-
-
-
-
-                    //string change = changeValue.Substring(0, 5).Trim();
-
-                    // Console.WriteLine("change: " + change);
-
-                    // HtmlNodeCollection cells = node.SelectNodes("td");
-                    /*
-                    int cellsCount = cells.Count;
-
-                    foreach (HtmlNode cell in cells)
-                    {
-                        // Console.WriteLine("cell: " + cell.InnerText);
-                    }
-                    */
-                    /*
-                    for (int cell = 0; cell < cellsCount; cell++)
-                    {
-                        string symbolData = cells.ElementAt(0).InnerText;
-                        Console.WriteLine("Symbol: " + symbolData);
-                        string nameData= cells.ElementAt(1).InnerText;
-                        Console.WriteLine("Name: " + symbolData);
-                        string indexData = cells.ElementAt(2).InnerText;
-                        Console.WriteLine("Index: " + symbolData);
-                        /*
-                        string changeData = cells[3].InnerText;
-                        string netPercentData = cells[4].InnerText;
-                        string highData = cells[5].InnerText;
-                        string lowData = cells[6].InnerText;
-                        DateTime timestampData = DateTime.Now;
-                        
-
-                    }
-                    */
-
-
-
-                    /*
-                    foreach (HtmlNode cell in cells)
-                    {
-                        Console.WriteLine("cell: " + cell.InnerText);
-                    }
-                    */
-
-                    /*
-                    foreach (var cell in node.SelectNodes("//*[@id=\"left-column-div\"]/div[4]/table/tbody"))
-                    {
-
-                    }
-                    */
+                    context.SaveChanges();                   
                 }
-                /*
-                var stockRecord = new NasdaqStock
-                {
-                    Symbol = symbolData,
-                    LastPrice = lastPriceData,
-                    Change = changeData,
-                    ChangeRate = changeRateData,
-                    Currency = currencyData,
-                    MarketTime = marketTimeData,
-                    Volume = volumeData,
-                    Shares = shareData,
-                    AverageVolume = averageVolumeData,
-                    MarketCap = marketCapData
-                };
-                context.Stocks.Add(stockRecord);
-                context.SaveChanges();
-
-                */
             }
         }
 
